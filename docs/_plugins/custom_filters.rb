e9ceae5f4c31f7cef1ad0c67af7a08d908611c6d
SOTR654 <r.sotr654@gmail.com>
2025-01-05 13:46:14 -0300

    filter

--- docs/_plugins/custom_filters.rb
+++ docs/_plugins/custom_filters.rb
@@ -1,10 +1,10 @@
-module CustomFilters
+module Jekyll
+  module CustomFilters
     def custom_replace(input)
       input
-        .gsub(" Sheet ", "<a href='https://sotr654.github.io/copy/Objects/Sheet.md'> <code>Sheet</code> </a>")
-        .gsub(" Cell ", " <code>Cell</code> ")
+      .gsub(" Sheet ", "<a href='https://sotr654.github.io/copy/Objects/Sheet.md'> <code>Sheet</code> </a>")
+      .gsub(" Cell ", " <code>Cell</code> ")
     end
   end
-  
-  Liquid::Template.register_filter(CustomFilters)
-  
+end  
+Liquid::Template.register_filter(Jekyll::CustomFilters)
